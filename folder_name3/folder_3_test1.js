4cf9d8ded919ec4476336dc9ca419f8bea8ef8e9
dinodevkma <dinodevkma@outlook.com>
2020-06-23 19:38:49 -0700

    added folder_name3

---
+++ folder_name3/folder_3_test1.js
@@ -0,0 +1,9 @@
+function my_function(data, result) {
+
+	// do something (it'll take sometime, you can use setTimeout() here, so that delay the time, for example.
+
+	result.folder3_test1 = 'test1';
+	return result;
+}
+
+module.exports = my_function;
